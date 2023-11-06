9bf0c7382c1877c42da7f41bdd778b84d4b739b0
Олексій Тригуб <149113900+Flairim@users.noreply.github.com>
2023-11-07 01:36:37 +0200

    task8.py

--- task8.py
+++ task8.py
@@ -12,17 +12,22 @@ def get_birthdays_per_week(users):
         birthday = user['birthday']
         days_until_birthday = (birthday - today).days
 
-        if days_until_birthday == 0:  
+        if days_until_birthday < 0:
+            days_until_birthday += 365
+
+        if days_until_birthday == 0:
             day_to_greet = current_day_of_week
         else:
             day_to_greet = (current_day_of_week + days_until_birthday) % 7
 
-        birthdays_per_day[day_to_greet].append(name)
-        
-    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    for day_index in range(7):
-        day_name = days_of_week[day_index]
-        names = ', '.join(birthdays_per_day[day_index])
-        if names:
-            print(f"{day_name}: {names}")
-            
+        day_name = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        while day_name[day_to_greet] in ["Saturday", "Sunday"]:
+            day_to_greet = (day_to_greet + 1) % 7
+
+        birthdays_per_day[day_name[day_to_greet]].append(name)
+
+    if not any(birthdays_per_day.values()):
+        return {}
+    else:
+        return birthdays_per_day
+
